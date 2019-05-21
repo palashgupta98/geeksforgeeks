27f78f06fcdaeface75bf715cfe710fc8b509a42
Palash Gupta <palashg7563@gmail.com>
2019-05-21 08:08:16 +0530

    delete and insert head function is implemented

---
+++ src/Linklist/Linklist.js
@@ -0,0 +1,49 @@
+/* eslint-disable no-console */
+
+import Node from "./Node";
+
+class Linklist {
+  constructor() {
+    this.head = null;
+  }
+
+  /**
+   * @description used to print the linklist
+   */
+  print() {
+    let node = this.head;
+    while (node != null) {
+      console.log(node.value);
+      node = node.next;
+    }
+  }
+
+  insertAtHead(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+  }
+
+  /**
+   * @description iterative function to delete the key
+   * @param {Number} key
+   * @returns {Boolean}
+   */
+  deleteAtKey(key) {
+    let node = this.head;
+    if (node.value === key) {
+      this.head = this.head.next;
+      return true;
+    }
+    while (node.next != null) {
+      if (node.next.value === key) {
+        node.next = node.next.next;
+        break;
+      }
+      node = node.next;
+    }
+    return true;
+  }
+}
+
+export default Linklist;
