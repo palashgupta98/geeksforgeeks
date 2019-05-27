ea0c765c1ece5f8ef1b6b4f2d148966e854d06fe
Palash Gupta <palashg7563@gmail.com>
2019-05-27 09:42:51 +0530

    babel config is updated

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  presets: ["@babel/preset-env", "@babel/preset-flow"]
+  presets: ["@babel/preset-env", "@babel/preset-flow"],
+  plugins: ["@babel/plugin-proposal-class-properties"]
 };
