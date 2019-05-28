274e9072ac8fe2dddf3d6be0c5440ec7cd5dbad4
Palash Gupta <palashg7563@gmail.com>
2019-05-28 11:59:01 +0530

    js config are removed

--- src/Linklist/Node.test.js
+++
@@ -1,9 +0,0 @@
-import Node from "./Node";
-
-describe("Node", () => {
-  it("create node with value 1", () => {
-    const node = new Node(1);
-    expect(node.value).toEqual(1);
-    expect(node.next).toBeNull();
-  });
-});
