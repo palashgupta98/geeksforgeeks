209d22a156de7e6aac579e7959ed40cfd20bc7d5
Palash Gupta <palashg7563@gmail.com>
2019-05-28 11:59:48 +0530

    eslint jest prettier is config for ts

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,29 @@
 module.exports = {
   env: {
-    browser: true,
-    es6: true,
+    jest: true,
     node: true,
-    jest: true
   },
-  parser: "babel-eslint",
+  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+  plugins: ["jest"],
   extends: [
-    "airbnb-base",
-    "plugin:flowtype/recommended",
-    "plugin:prettier/recommended"
+    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    "plugin:prettier/recommended", // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    "plugin:jest/recommended",
   ],
-  plugins: ["flowtype"],
-  globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
-  },
   parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: "module"
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: "module", // Allows for the use of imports
   },
   rules: {
-    "flowtype/no-types-missing-file-annotation": "off"
-  }
+    "@typescript-eslint/explicit-member-accessibility": "off",
+  },
+  overrides: [
+    {
+      files: ["*.test.js", "*.spec.js", "*.test.ts", "*.spec.ts"],
+      rules: {
+        "@typescript-eslint/explicit-function-return-type": "off",
+      },
+    },
+  ],
 };
